3e6b37ab3f576e8d8dbd6a4d6b2dd830c82ec8d0
Jehiel Andru <160086094+Jehiel19Andru@users.noreply.github.com>
2026-02-18 14:00:31 -0600

    Add output directory configuration for build

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Forzamos a que la carpeta de salida sea 'dist'
+    outDir: 'dist',
+  }
 })
